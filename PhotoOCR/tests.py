c7369aa0eb603fe0ac416d7a5dcae3e122b60b63
sunqinghao <sunqinghao@sunqinghao-desktop>
2014-12-15 14:11:05 +0800

    Modified:	add process steps.

--- PhotoOCR/tests.py
+++ PhotoOCR/tests.py
@@ -4,7 +4,6 @@ Created on 2014年12月14日
 
 @author: leonardo
 '''
-import numpy as np
 from Source.neurals.cortex import Cortex, NeuralRelation
 from Source.neurals.indexable import NestingArrayIndexable
 
@@ -37,3 +36,43 @@ for y in range(level_1_cortex.shape):
     level_2_data.append(row)
     
 level_2_cortex = Cortex(NestingArrayIndexable(level_2_data, level_1_cortex.shape))
+
+content = []
+for y_index in level_2_cortex.shape[1]:
+    for x_index in level_2_cortex.shape[0]:
+            rel_neu = level_2_cortex.get(x_index, y_index)
+            # define background
+            if ((rel_neu.up is None or rel_neu.up == 0) and
+                (rel_neu.down is None or rel_neu.down == 0) and
+                (rel_neu.left is None or rel_neu.left == 0) and
+                (rel_neu.right is None or rel_neu.right == 0) and
+                (rel_neu.up_left is None or rel_neu.up_left == 0) and
+                (rel_neu.down_left is None or rel_neu.down_left == 0) and
+                (rel_neu.up_right is None or rel_neu.up_right == 0) and
+                (rel_neu.down_right is None or rel_neu.down_right == 0)):
+                # background
+                pass
+            else:
+                # get content (wipe off background)
+                content.append(x_index, y_index)
+
+def Map(content):
+    pass
+
+# connect content (record related pattern)
+level_3_cortex = Map(content)
+
+# abstract connected pattern (include low level feature; count the appearance of these feature; etc.)
+
+# loop training set, get multiple data.
+
+# find difference
+
+# further abstract
+
+# basically, we have a point to represent pen ink.
+# using machine to write number with this point.
+# before writing, we learn the basic structure of numbers.
+# and the character randomly and slightly differ from the structure.
+# after writing a lot examples, we can write properly,
+# and can recognize hand writing as well.
